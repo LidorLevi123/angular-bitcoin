c6bae11a2206c2f6957e96c9c241e2cae9a5ccbb
LidorLevi123 <lidorlevi728@gmail.com>
2023-08-29 20:50:25 +0300

    add contact page

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-bitcoin';
+  private contactService = inject(ContactService)
+
+  ngOnInit(): void {
+    this.contactService.loadContacts().subscribe({ error: err => console.log(err) })
+  }
 }
--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/models/contact.model';
+import { Observable } from 'rxjs'
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss']
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit {
 
+  constructor(private contactService: ContactService) { }
+  contacts$!: Observable<Contact[]>
+
+  ngOnInit(): void {
+    this.contacts$ = this.contactService.contacts$
+  }
 }
