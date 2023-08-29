1a356e26559c8a33a924d3db0232786e5cc19520
LidorLevi123 <lidorlevi728@gmail.com>
2023-08-30 01:58:02 +0300

    logo clicking take to home page

--- src/app/cmps/loader/loader.component.ts
+++ src/app/cmps/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'loader',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
-
+  constructor(private loaderService: LoaderService) { }
+  isLoading$ = this.loaderService.isLoading$
 }
